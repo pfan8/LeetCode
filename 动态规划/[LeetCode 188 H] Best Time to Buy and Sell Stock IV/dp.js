9a9e989b40e4f73fe47adc541cb0baf03e804d60
Kun Hades <fqwgc8@outlook.com>
2023-12-31 16:23:03 +0800

    feat: buy and sell stock IV solution

--- 动态规划/[LeetCode 188 H] Best Time to Buy and Sell Stock IV/dp.js
+++ 动态规划/[LeetCode 188 H] Best Time to Buy and Sell Stock IV/dp.js
@@ -1,21 +1,30 @@
 /**
+ * @param {number} k
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-  // d[i][0] = max(d[i-1][0], d[i-1][1] + price[i])
-  // d[i][1] = max(d[i-1][1], d[i-2][0] - price[i])
-  if (prices.length < 2) return 0;
-  let dp = [...Array(prices.length)].map((x) => Array(2).fill(0));
-  dp[0][0] = 0;
-  dp[0][1] = -prices[0];
-  dp[1][0] = Math.max(0, dp[0][1] + prices[1]);
-  dp[1][1] = Math.max(dp[0][1], -prices[1]);
+var maxProfit = function (k, prices) {
+  const len = prices.length;
+  const remain = k;
+  const dp = [...Array(len + 1)].map(() =>
+    [...Array(remain + 1)].map(() => [...Array(2)].fill(0))
+  );
 
-  for (let i = 2; i < prices.length; i += 1) {
-    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+  for (let i = 0; i <= remain; i++) {
+    dp[0][i][1] = i === remain - 1 ? -prices[0] : -10e5;
   }
 
-  return Math.max(dp[prices.length - 1][0], dp[prices.length - 2][0]);
+  for (let i = 0; i < len; i++) {
+    // no remain count
+    for (let j = remain - 1; j >= 0; j--) {
+      dp[i + 1][j][0] = Math.max(dp[i][j][0], dp[i][j][1] + prices[i]);
+      dp[i + 1][j][1] = Math.max(dp[i][j + 1][0] - prices[i], dp[i][j][1]);
+    }
+  }
+
+  let ans = 0;
+  for (let i = 0; i <= remain; i++) {
+    ans = Math.max(ans, dp[len][i][0], dp[len][i][1]);
+  }
+  return ans;
 };
